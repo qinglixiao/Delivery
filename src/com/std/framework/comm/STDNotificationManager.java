b50d9bd43cb5b33f7c5f55a2d7e6e309c28dc25c
qinglixiao <qinglixiao@users.noreply.github.com>
2015-02-07 18:48:16 +0800

    Update STDNotificationManager.java

--- src/com/std/framework/comm/STDNotificationManager.java
+++ src/com/std/framework/comm/STDNotificationManager.java
@@ -5,15 +5,21 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
+import com.std.framework.R;
+import com.std.framework.activity.MainActivity;
+
 
 public class STDNotificationManager {
 	private static STDNotificationManager instance;
 	private Context context;
 	private NotificationManager notificationManager;
+	private RemoteViews defContenView;
+	private PendingIntent defPendingIntent;
 	
 	private STDNotificationManager(Context context){
 		this.context = context;
-		notificationManager =  (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		init();
 	}
 	
 	public static STDNotificationManager getInstance(Context context){
@@ -27,11 +33,23 @@ public class STDNotificationManager {
 		return instance;
 	}
 	
-	public void notify(Intent intent){
-		Notification notification = new Notification();
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-		notification.contentIntent = pendingIntent;
+	private void init(){
+		notificationManager =  (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		defContenView =  new RemoteViews(context.getPackageName(), R.layout.notification);
+		defPendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+	}
+	
+	public void notify(String message){
+		Notification notification = newNotification(defContenView,defPendingIntent);
+		notification.contentView.setTextViewText(R.id.notification_tv_message, message);
 		notificationManager.notify(0, notification);
+	}
+	
+	private Notification newNotification(RemoteViews contentViews,PendingIntent pendingIntent){
+		Notification notification = new Notification();
+		notification.contentView = contentViews;
+		notification.contentIntent = pendingIntent;
+		return notification;
 	}
 	
 }
