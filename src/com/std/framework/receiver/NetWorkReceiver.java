c7a7bf0b2fd47c461411eaecbbcc91aeea343bf3
Administrator <Administrator@lx-PC>
2015-02-07 18:51:53 +0800

    Signed-off-by: Administrator <Administrator@lx-PC>

---
+++ src/com/std/framework/receiver/NetWorkReceiver.java
@@ -0,0 +1,15 @@
+package com.std.framework.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetWorkReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
