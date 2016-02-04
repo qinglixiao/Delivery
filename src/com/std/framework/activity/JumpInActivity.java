d4cad0883de4aa1414dcb8b0d0df8ada95fff01c
137289 <lixiao3@syswin.com>
2016-02-04 15:42:17 +0800

    修改配置文件

--- src/com/std/framework/activity/JumpInActivity.java
+++ src/com/std/framework/activity/JumpInActivity.java
@@ -1,7 +1,16 @@
 package com.std.framework.activity;
 
+import android.os.Bundle;
+
+import com.std.framework.R;
+
 /**
  * Created by Administrator on 2016/1/19.
  */
-public class JumpInActivity {
+public class JumpInActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.jumpinactivity);
+    }
 }
