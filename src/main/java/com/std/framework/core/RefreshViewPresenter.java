4b47559f4062f7a49c2c4f7b522eaf42b5882699
137289 <lixiao3@syswin.com>
2016-05-12 18:24:15 +0800

    更新功能

---
+++ src/main/java/com/std/framework/core/RefreshViewPresenter.java
@@ -0,0 +1,7 @@
+package com.std.framework.core;
+
+/**
+ * Created by gfy on 2016/5/6.
+ */
+public interface RefreshViewPresenter {
+}
---
+++ src/main/java/com/std/framework/core/StdView.java
@@ -0,0 +1,8 @@
+package com.std.framework.core;
+
+/**
+ * Created by gfy on 2016/5/6.
+ */
+public interface StdView extends BaseView {
+    void show();
+}
