911a1eca659005084ef08f48d86d4ff68ab6ead2
qinglixiao <qinglixiao@163.com>
2016-08-12 11:41:33 +0800

    更新底部导航条

--- src/main/java/com/std/framework/main/view/activity/MainTabActivity.java
+++ src/main/java/com/std/framework/main/view/activity/MainTabActivity.java
@@ -1,66 +1,54 @@
 package com.std.framework.main.view.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TabHost.OnTabChangeListener;
-import android.widget.TextView;
 
 import com.std.framework.R;
 import com.std.framework.basic.BaseTitleActivity;
 import com.std.framework.core.NavigationBar;
-import com.std.framework.main.view.fragment.FiveFragment;
-import com.std.framework.main.view.fragment.FourFragment;
+import com.std.framework.databinding.ActivityTabMain;
 import com.std.framework.main.view.fragment.MainFragment;
 import com.std.framework.main.view.fragment.SecondFragment;
-import com.std.framework.main.view.fragment.ThreeFragment;
+import com.std.framework.view.MainBottomView;
 
-public class MainTabActivity extends BaseTitleActivity implements OnTabChangeListener {
-    private static final String PAGE_ONE = "page_one";
-    private static final String PAGE_TWO = "page_two";
-    private static final String PAGE_THREE = "page_three";
-    private static final String PAGE_FOUR = "page_four";
-    private static final String PAGE_FIVE = "page_five";
+public class MainTabActivity extends BaseTitleActivity {
+    private ActivityTabMain activityTabMain;
 
-    private FragmentTabHost mTabHost;
+    private MainBottomView.TabSpec listTab;
+    private MainBottomView.TabSpec contactTab;
+    private MainBottomView.TabSpec homeTab;
+    private MainBottomView.TabSpec communicateTab;
+    private MainBottomView.TabSpec settingTab;
 
     @Override
     protected void onCreate(Bundle arg0) {
         // TODO Auto-generated method stub
         super.onCreate(arg0);
-        setContentView(R.layout.activity_tab_main);
-        initViewTab();
+        View view = View.inflate(this, R.layout.activity_tab_main, null);
+        activityTabMain = DataBindingUtil.bind(view);
+        setContentView(view);
+        initTab();
     }
 
-    private void initViewTab() {
-        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+    private void initTab() {
+        //订单列表
+        listTab = activityTabMain.tabContainer.newTabSpec(0, R.drawable.main_app_list, R.string.main_tab_list, MainFragment.class);
+        activityTabMain.tabContainer.addTab(listTab);
+        //通讯录
+        contactTab = activityTabMain.tabContainer.newTabSpec(1, R.drawable.main_app_contact, R.string.main_tab_contact, SecondFragment.class);
+        activityTabMain.tabContainer.addTab(contactTab);
+        //主页
+        homeTab = activityTabMain.tabContainer.newTabSpec(2, R.drawable.main_app_home, R.string.main_tab_home, SecondFragment.class);
+        activityTabMain.tabContainer.addTab(homeTab);
+        //沟通
+        communicateTab = activityTabMain.tabContainer.newTabSpec(3, R.drawable.main_app_communicate, R.string.main_tab_communicate, SecondFragment.class);
+        activityTabMain.tabContainer.addTab(communicateTab);
+        //设置
+        settingTab = activityTabMain.tabContainer.newTabSpec(4, R.drawable.main_app_setting, R.string.main_tab_setting, SecondFragment.class);
+        activityTabMain.tabContainer.addTab(settingTab);
 
-        mTabHost.addTab(mTabHost.newTabSpec(PAGE_ONE).setIndicator(
-                newIndicator(R.drawable.ali144, "one")), MainFragment.class, null);
-
-        mTabHost.addTab(mTabHost.newTabSpec(PAGE_TWO).setIndicator(
-                newIndicator(R.drawable.ali144, "two")), SecondFragment.class, null);
-
-        mTabHost.addTab(mTabHost.newTabSpec(PAGE_THREE).setIndicator(
-                newIndicator(R.drawable.ali144, "three")), ThreeFragment.class, null);
-
-        mTabHost.addTab(mTabHost.newTabSpec(PAGE_FOUR).setIndicator(
-                newIndicator(R.drawable.ali144, "four")), FourFragment.class, null);
-
-        mTabHost.addTab(mTabHost.newTabSpec(PAGE_FIVE).setIndicator(
-                newIndicator(R.drawable.ali144, "five")), FiveFragment.class, null);
-
-        mTabHost.setOnTabChangedListener(this);
-        mTabHost.setCurrentTabByTag(PAGE_THREE);
-    }
-
-    private View newIndicator(int imgResId, String text) {
-        View view = View.inflate(this, R.layout.tab_wiget_item, null);
-        ((ImageView) view.findViewById(R.id.tab_img)).setImageResource(imgResId);
-        ((TextView) view.findViewById(R.id.tab_tv)).setText(text);
-        return view;
+        activityTabMain.tabContainer.apply(activityTabMain.tabContent,this);
     }
 
     @Override
@@ -70,14 +58,4 @@ public class MainTabActivity extends BaseTitleActivity implements OnTabChangeLis
         navigation.setNavigationIcon(R.drawable.icon);
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        mTabHost = null;
-    }
-
-    @Override
-    public void onTabChanged(String tabId) {
-        // TODO Auto-generated method stub
-        mTabHost.getCurrentTabView().findViewById(R.id.tab_adorn).setVisibility(View.GONE);
-    }
 }
--- src/main/java/com/std/framework/view/MainBottomView.java
+++ src/main/java/com/std/framework/view/MainBottomView.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -13,11 +18,12 @@ import android.widget.TextView;
 
 import com.library.core.Reflect;
 import com.std.framework.R;
-import com.std.framework.basic.App;
 import com.std.framework.databinding.BottomTabLayout;
 import com.std.framework.fragment.BaseFragment;
+import com.std.framework.view.MainBottomView.TabSpec.OnTabClickListener;
 
-import okhttp3.Request;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description :
@@ -25,10 +31,19 @@ import okhttp3.Request;
  * Create on: 2016/7/13
  * Modify by：lx
  */
-public class MainBottomView extends LinearLayout {
+public class MainBottomView extends LinearLayout{
+    protected List<TabSpec> tabs = new ArrayList();
+
+    private ViewPager viewPager;
+
+    private int cur_index = 0;
 
     public MainBottomView(Context context) {
         super(context);
+    }
+
+    public MainBottomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
     public TabSpec newTabSpec(int index, @DrawableRes int iconDrawableId, @StringRes int iconNameId, Class<? extends BaseFragment> clazz) {
@@ -37,6 +52,7 @@ public class MainBottomView extends LinearLayout {
                 .setStrId(iconNameId)
                 .setFragmentClass(clazz)
                 .setIndex(index)
+                .setContext(getContext())
                 .build();
     }
 
@@ -44,61 +60,108 @@ public class MainBottomView extends LinearLayout {
         LayoutParams lp = new LayoutParams(0, LayoutParams.WRAP_CONTENT);
         lp.weight = 1;
         lp.gravity = Gravity.CENTER;
-        addView(tabSpec.getView(),lp);
+        addView(tabSpec.getView(), lp);
+        tabs.add(tabSpec);
+        tabSpec.setOnTabClickListener(tabClickListener);
     }
 
-    public static class TabSpec implements OnClickListener {
+    public void setDefaultSelected(int index) {
+        cur_index = index;
+    }
+
+    public <T extends FragmentActivity> void apply(ViewPager viewPager, T context) {
+        if (viewPager != null) {
+            viewPager.setAdapter(new TabAdapter(context.getSupportFragmentManager()));
+            viewPager.setCurrentItem(cur_index);
+        }
+    }
+
+    private OnTabClickListener tabClickListener = new OnTabClickListener() {
+        @Override
+        public void onClick(TabSpec tabSpec) {
+            if(viewPager != null){
+                viewPager.setCurrentItem(tabSpec.getIndex());
+            }
+        }
+    };
+
+    public class TabAdapter extends FragmentPagerAdapter {
+
+        public TabAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return tabs.get(position).getFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return tabs.size();
+        }
+
+    }
+
+    public static class TabSpec {
         private int drawableId;
         private int strId;
         private int index;
-        private BottomTabLayout bottomTabLayout;
         private Class<? extends BaseFragment> fragmentClass;
         private OnTabClickListener onTabClickListener;
+        private Context context;
+        private BottomTabLayout bottomTabLayout;
+        private View view;
 
-        private TabSpec(int index,int drawableId, int strId, Class<? extends BaseFragment> clazz) {
+        private TabSpec(int index, int drawableId, int strId, Class<? extends BaseFragment> clazz, Context context) {
             this.drawableId = drawableId;
             this.strId = strId;
             this.fragmentClass = clazz;
             this.index = index;
+            this.context = context;
         }
 
         protected View getView() {
-            View view = View.inflate(App.instance, R.layout.bottom_tab_layout, null);
-            BottomTabLayout bottomTabLayout = DataBindingUtil.bind(view);
-            bottomTabLayout.imgTabIcon.setImageResource(drawableId);
-            bottomTabLayout.tvTabName.setText(strId);
-            bottomTabLayout.tabMain.setOnClickListener(this);
+            if(view == null){
+                view = View.inflate(context, R.layout.bottom_tab_layout, null);
+                BottomTabLayout bottomTabLayout = DataBindingUtil.bind(view);
+                bottomTabLayout.imgTabIcon.setImageResource(drawableId);
+                bottomTabLayout.tvTabName.setText(strId);
+                bottomTabLayout.tabMain.setOnClickListener(onClickListener);
+            }
             return view;
         }
 
-        public ImageView getIconImageView(){
+        public ImageView getIconImageView() {
             return bottomTabLayout.imgTabIcon;
         }
 
-        public TextView getIconTextView(){
+        public TextView getIconTextView() {
             return bottomTabLayout.tvTabName;
         }
 
-        public void setOnTabClickListener(OnTabClickListener onTabClickListener){
+        public void setOnTabClickListener(OnTabClickListener onTabClickListener) {
             this.onTabClickListener = onTabClickListener;
         }
 
-        public int getIndex(){
+        public int getIndex() {
             return index;
         }
 
-        public BaseFragment getFragment(){
+        public BaseFragment getFragment() {
             return Reflect.on(fragmentClass).create().get();
         }
 
-        @Override
-        public void onClick(View v) {
-            if(onTabClickListener != null){
-                onTabClickListener.onClick(this);
+        private OnClickListener onClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onTabClickListener != null) {
+                    onTabClickListener.onClick(TabSpec.this);
+                }
             }
-        }
+        };
 
-        interface OnTabClickListener{
+        interface OnTabClickListener {
             void onClick(TabSpec tabSpec);
         }
 
@@ -108,6 +171,8 @@ public class MainBottomView extends LinearLayout {
             private int strId;
 
             private int index;
+
+            private Context context;
 
             private Class<? extends BaseFragment> fragmentClass;
 
@@ -126,13 +191,18 @@ public class MainBottomView extends LinearLayout {
                 return this;
             }
 
-            public Builder setIndex(int index){
+            public Builder setIndex(int index) {
                 this.index = index;
                 return this;
             }
 
+            public Builder setContext(Context context) {
+                this.context = context;
+                return this;
+            }
+
             public TabSpec build() {
-                return new TabSpec(index, drawableId, strId, fragmentClass);
+                return new TabSpec(index, drawableId, strId, fragmentClass, context);
             }
         }
     }
