e7db0afddb801c596b434521220da777aa446533
qinglixiao <qinglixiao@163.com>
2016-09-05 11:24:31 +0800

    去除多余接口

--- src/main/java/com/std/framework/business/find/view/fragment/FindFragment.java
+++ src/main/java/com/std/framework/business/find/view/fragment/FindFragment.java
@@ -19,12 +19,20 @@ import com.std.framework.comm.view.PagerSlidingTabStrip;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class FindFragment extends BaseFragment implements ViewPager.OnPageChangeListener {
+public class FindFragment extends BaseFragment {
     private View view;
     private ViewPager viewPager;
     private PagerSlidingTabStrip tabStrip;
     private MyPagerAdapter adapter = null;
-    private Map<String, Class<? extends BaseFragment>> fragments;
+    private static Map<String, Class<? extends BaseFragment>> fragments;
+
+    static {
+        fragments = new LinkedHashMap<>();
+        fragments.put("EventBus", EventBusFragment.class);
+        fragments.put("RxBus", AnimationFragment.RxBusFragment.class);
+        fragments.put("CustomerView", CustomerViewFragment.class);
+        fragments.put("Animation", AnimationFragment.class);
+    }
 
     @Override
     public void onNavigationBar(NavigationBar navigationBar) {
@@ -33,14 +41,12 @@ public class FindFragment extends BaseFragment implements ViewPager.OnPageChange
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        initFragment();
         if (view == null) {
             view = inflater.inflate(R.layout.fragment_second, null);
             viewPager = (ViewPager) view.findViewById(R.id.pager);
             tabStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
             tabStrip.setAllCaps(false);
             initPager();
-            tabStrip.setOnPageChangeListener(this);
         }
         ViewGroup parent = (ViewGroup) view.getParent();
         if (parent != null)
@@ -48,33 +54,10 @@ public class FindFragment extends BaseFragment implements ViewPager.OnPageChange
         return view;
     }
 
-    private void initFragment() {
-        fragments = new LinkedHashMap<>();
-        fragments.put("EventBus", EventBusFragment.class);
-        fragments.put("RxBus", AnimationFragment.RxBusFragment.class);
-        fragments.put("CustomerView", CustomerViewFragment.class);
-        fragments.put("Animation", AnimationFragment.class);
-    }
-
     private void initPager() {
         adapter = new MyPagerAdapter(getFragmentManager());
         viewPager.setAdapter(adapter);
         tabStrip.setViewPager(viewPager);
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        Logger.m(adapter.getItem(position).getClass().getSimpleName());
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
     }
 
     class MyPagerAdapter extends FragmentPagerAdapter {
