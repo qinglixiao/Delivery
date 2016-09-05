828954470dcaa029b5bbdac49db3dfa717396fb6
qinglixiao <qinglixiao@163.com>
2016-09-05 18:52:56 +0800

    群呼功能开发

--- src/main/java/com/std/framework/business/call/view/fragment/CallFragment.java
+++ src/main/java/com/std/framework/business/call/view/fragment/CallFragment.java
@@ -1,6 +1,7 @@
 package com.std.framework.business.call.view.fragment;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.parser.BindingExpressionParser;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,6 +12,13 @@ import com.std.framework.R;
 import com.std.framework.basic.BaseFragment;
 import com.std.framework.core.NavigationBar;
 import com.std.framework.databinding.FragmentCallBinding;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class CallFragment extends BaseFragment {
     private FragmentCallBinding fragmentCallBinding;
@@ -25,6 +33,18 @@ public class CallFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_call, null);
         fragmentCallBinding = DataBindingUtil.bind(view);
         return view;
+    }
+
+    private void playImitate() {
+        Observable
+                .interval(1000, TimeUnit.MILLISECONDS, Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+
+                    }
+                });
     }
 
 
