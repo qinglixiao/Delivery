6c56887772c5826aad9bd180d64f326eee9c3bce
qinglixiao <qinglixiao@163.com>
2016-09-29 18:15:43 +0800

    add recyclerview decoration

--- src/main/java/com/std/framework/business/explore/view/fragment/RecyclerViewFragment.java
+++ src/main/java/com/std/framework/business/explore/view/fragment/RecyclerViewFragment.java
@@ -3,6 +3,7 @@ package com.std.framework.business.explore.view.fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -39,6 +40,7 @@ public class RecyclerViewFragment extends BaseFragment {
     private void initView(){
         recyclerView = recyclerViewBinding.rcvResult;
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     private void initData(){
