01388861194844b83d79412b6131a280c58da00c
qinglixiao <qinglixiao@163.com>
2016-12-10 10:56:50 +0800

    录制音频

---
+++ src/main/java/com/std/framework/core/VoiceRecordHelper.java
@@ -0,0 +1,14 @@
+package com.std.framework.core;
+
+/**
+ * Description:
+ * Created by 李晓 ON 2016/12/8.
+ * Job number:137289
+ * Phone:18611867932
+ * Email:lixiao3@syswin.com
+ * Person in charge:李晓
+ * Leader:
+ */
+public class VoiceRecordHelper {
+
+}
---
+++ src/main/java/com/std/framework/util/ToastUtil.java
@@ -0,0 +1,13 @@
+package com.std.framework.util;
+
+/**
+ * Description:
+ * Created by 李晓 ON 2016/12/8.
+ * Job number:137289
+ * Phone:18611867932
+ * Email:lixiao3@syswin.com
+ * Person in charge:李晓
+ * Leader:
+ */
+public class ToastUtil {
+}
